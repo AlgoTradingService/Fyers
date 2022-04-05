ee2bc9915bdf553d1fa3d246921046b0d26dbf0c
Nikhil Thakur <thakurnikhilsingh1@gmail.com>
2022-04-05 23:47:28 +0530

    Added threading.

--- test.py
+++ test.py
@@ -1,27 +1,39 @@
 from time import time
 from interact_package import get_profile
-import sys
+import sys, select
 
+TIMEOUT = 5
 import threading
 exit = ""
 
 def excecute():
-    
+    global exit
     if exit == "EXIT":
         sys.exit(2)
     else:
         print(get_profile.profile())
-        timer = threading.Timer(5, excecute)
+        timer = threading.Timer(10, excecute) # Executing this thread for every 10 seconds.
+        timer.start()
 
 
-def take_user_input():
-    user_input = input("Do you wanna continue ?")
+def user_input():
     global exit
-    exit = user_input
-    timer2 = threading.Timer(1 , take_user_input)
+    while True:
+        print("If you want to exit, type 'EXIT' and press Enter.")
 
+        i, o, e = select.select([sys.stdin], [], [], 5)
+        if i:
+            ui = sys.stdin.readline().strip()
+            if ui == 'EXIT':
+                exit = ui
+                sys.exit(4)
 
 excecute()
 
-take_user_input()
+user_input()
+
+ 
+
+
+
 
