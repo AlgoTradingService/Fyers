3c714815310c131683d6e037745d45c5dd2ea57f
Nikhil Thakur <thakurnikhilsingh1@gmail.com>
2022-04-08 17:00:39 +0530

    Added code to traverse trough a folder tree and get all .csv files

---
+++ test/test_csv_read.py
@@ -0,0 +1,20 @@
+import os
+
+folder_path = '/home/nikhil/Documents/Fyers/data'
+def traverse_files_from_a_folder(folder_path):
+    """Pass the path of the folder. It will
+    return a list of all files contains in
+    that folder tree."""
+    files = []
+    path = folder_path
+    for item in os.listdir(folder_path):
+        
+        if os.path.isdir(path +'/'+item):
+            print('folder',item)
+            files.extend(traverse_files_from_a_folder(path +'/'+item))
+        else:
+            print('file',item)
+            files.append(item)
+    return files
+        
+traverse_files_from_a_folder(folder_path)
